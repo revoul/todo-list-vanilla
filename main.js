85c1e8f0ecb88f58281d80eac2070aec772547de
Paul <lordizmur@gmail.com>
2019-12-04 09:29:11 +0100

    chore: add exercises

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+	[ ] - Al pulsar en el botón #addBtnId se debe añadir lo que hay en el campo #titleInput.
+	[ ] - Tras el caso anterior, también se debe reiniciar el campo #titleInput para quedarse vacío.
+	[ ] - Ahora también quiero poder borrar un elemento de la lista, lo haremos añadiendo un botón dentro de cada uno de los <li>
+		quedando de la siguiente manera: <li>Title <button type="button" class="close">x</button></li>.
+		Este botón, tras ser pulsado debe eliminar dicho elemento.
+	[ ] - Queremos que tras pulsar un elemento <li> del listado, este se tache. Para que esto sea posible,
+		tendremos que añadir la clase 'checked' al elemento <li>.
+*/
